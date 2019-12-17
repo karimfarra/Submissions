5b8d544c1d0ff8e9eb27d9a256b739d62d15f8c0
karim <karimo199812@hotmail.com>
2019-12-17 22:16:02 +0200

    step 5 done

---
+++ Exercises/Javascript-Basics-02/step 5/step5.js
@@ -0,0 +1,19 @@
+function mouseOver() {
+  document.querySelector("#image1").addEventListener("mouseover", e => {
+    document.querySelector("#image1").src = "images/image1_2.jpg";
+  });
+  document.querySelector("#image2").addEventListener("mouseover", e => {
+    document.querySelector("#image2").src = "images/image2_2.jpg";
+  });
+  document.querySelector("#image3").addEventListener("mouseover", e => {
+    document.querySelector("#image3").src = "images/image3_2.jpg";
+  });
+  document.querySelector("#image4").addEventListener("mouseover", e => {
+    document.querySelector("#image4").src = "images/image4_2.jpg";
+  });
+  document.querySelector("#image5").addEventListener("mouseover", e => {
+    document.querySelector("#image5").src = "images/image5_2.jpg";
+  });
+}
+
+mouseOver();
