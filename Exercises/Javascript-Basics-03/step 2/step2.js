214b7a903739a494ceace332290a3a0479184f85
karim <karimo199812@hotmail.com>
2019-12-18 12:35:04 +0200

    completed step 2

---
+++ Exercises/Javascript-Basics-03/step 2/step2.js
@@ -0,0 +1,6 @@
+document.getElementById("hide").addEventListener("click", () => {
+  document.querySelector("#texte").style.display = "none";
+});
+document.getElementById("show").addEventListener("click", () => {
+  document.querySelector("#texte").style.display = "block";
+});
