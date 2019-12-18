60979ef39be9b82c2f139ccaf46fa6b1ab19d866
karim <karimo199812@hotmail.com>
2019-12-18 12:45:15 +0200

    completed step3

---
+++ Exercises/Javascript-Basics-03/step3.js/step3.js
@@ -0,0 +1,9 @@
+document.querySelector(".red").addEventListener("click", () => {
+  document.querySelector("p").style.color = "red";
+});
+document.querySelector(".blue").addEventListener("click", () => {
+  document.querySelector("p").style.color = "blue";
+});
+document.querySelector(".green").addEventListener("click", () => {
+  document.querySelector("p").style.color = "green";
+});
