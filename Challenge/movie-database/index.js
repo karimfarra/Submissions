dfb9f6b87a3d2a431df7585290bc57fb1c24aa57
karim <karimo199812@hotmail.com>
2020-01-23 11:42:33 +0200

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,4 +5,15 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  var d = new Date();
+  var t1 = d.getHours();
+  var t2 = d.getMinutes();
+  res.send({ status: 200, message: `${t1} : ${t2}` });
+});
+
 app.listen(3000, () => console.log("example app listening on port 3000"));
