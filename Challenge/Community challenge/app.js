25d38360376d4fedb0a2b23242fce201602fafa4
karim <karimo199812@hotmail.com>
2020-01-26 18:46:38 +0200

    completed community project

--- Challenge/Community challenge/app.js
+++ Challenge/Community challenge/app.js
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
-</head>
+import React, { Component } from "react";
+import "./style.css";
+
+
 <body>
     <header class="header">
       <div class="websitename">
@@ -55,4 +50,31 @@
 
       </main>
 </body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
